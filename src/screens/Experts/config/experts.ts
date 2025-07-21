cc6248caf8f7b51170ab380e035984b87a24fa3e
shishel-zaitcevich <88749942+shishel-zaitcevich@users.noreply.github.com>
2025-07-21 14:00:45 +0300

    Slider (#22)
    * fix
    
    * slider
    
    * fix slider
    
    * added animation to innovation cards
    
    * fix
    
    * innovation cards
    
    * added animation to innivations
    
    * added animations
    
    * fix
    
    * form
    
    * links
    
    * fix
    
    * added links to innovations
    
    * fix
    
    * fix
    
    * fixed button
    
    * fix
    
    * fix
    
    * fix
    
    * added styles
    
    * fix
    
    * fix
    
    * added slider, fixed innovations block
    
    * added map
    
    * fix
    
    * fix
    
    ---------
    
    Co-authored-by: shishel-zaitcevich <ushka_mail.ru>

--- src/screens/Experts/config/experts.ts
+++ src/screens/Experts/config/experts.ts
@@ -1,18 +1,20 @@
 export const experts = [
-  {
-    name: 'Алексей Зенин',
-    role: 'Директор, Бортовые системы и сервисы данных',
-   photoSrc: '/img/experts/zenin.png',
-  },
+
   {
     name: 'Никита Сиваков',
     role: 'Менеджер проектов разработки',
     photoSrc: '/img/experts/sivakov.png',
   },
+
   {
     name: 'Роман Яковишин',
     role: 'Начальник отдела продаж бортовых систем',
     photoSrc: '/img/experts/jakovishin.png',
+  },
+      {
+    name: 'Евгений Корянов',
+    role: 'CTO/System Architect',
+    photoSrc: '/img/experts/koryanov.png',
   },
   {
     name: 'Андрей Олейников',
